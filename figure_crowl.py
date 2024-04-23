324532977b01408dc90d0e922b38f98193151a67
chadtied <96424234+chadtied@users.noreply.github.com>
2024-04-24 00:54:17 +0800

    Update figure_crowl.py

--- figure_crowl.py
+++ figure_crowl.py
@@ -1,8 +1,16 @@
 from seleniumbase import Driver
 from selenium.webdriver.common.by import By
+import openpyxl
+from openpyxl.styles import Font
 
+
+
+workbook= openpyxl.Workbook()
+workpage= workbook.create_sheet("List1",0)
 imfor= dict()
 driver= Driver(uc= True, incognito= False)
+red_font= Font(color= 'FF0000')
+green_font= Font(color= '00FF00')
 
 try:
     driver.get("https://www.wantgoo.com/global")
@@ -63,3 +71,27 @@ for figure in figures:
         print("道瓊指數資料採掘錯誤")
 
 print(imfor)
+
+workpage.cell(1,1).value= "指標名稱"
+workpage.cell(1,2).value= "最近收盤價"
+workpage.cell(1,3).value= "漲跌點數"
+workpage.cell(1,4).value= "漲跌幅"
+row= 2
+for key, fig_dict in imfor.items():
+ workpage.cell(row,1).value= key
+ for fig_key,  fig_data in fig_dict.items():
+    if fig_key== 'CLOSE': 
+        workpage.cell(row,2).value= fig_data
+        if(fig_data>= 0):  workpage.cell(row,2).font= red_font
+        else: workpage.cell(row,2).font= green_font
+    elif fig_key== 'CHANGE':
+        workpage.cell(row,3).value= fig_data
+        if(fig_data>= 0):  workpage.cell(row,3).font= red_font
+        else: workpage.cell(row,2).font= green_font
+    elif fig_key== 'RATE': 
+        workpage.cell(row,4).value= fig_data
+        if(fig_data>= 0):  workpage.cell(row,4).font= red_font
+        else: workpage.cell(row,2).font= green_font
+ row+= 1
+
+ workbook.save("./new.xlsx")
